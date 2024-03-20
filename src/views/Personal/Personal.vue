4bded4baa1db16e82e144cde219c0afa53040839
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-03-21 01:36:36 +0800

    Update Personal.vue

--- src/views/Personal/Personal.vue
+++ src/views/Personal/Personal.vue
@@ -5,28 +5,22 @@
     icon="github-fill">
     <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
       <RepositoryItem
-        name="fridolph"
-        description="前端相关学习途径，综合性学习资料整理仓库"
-        link="https://github.com/Fridolph/Fridolph">
+        name="研导博客"
+        description="一个为广大学子提供导师信息的网站。"
+        link="https://rate.xiaoayu.ren">
         <div class="flex flex-row">
           <div class="flex-1">
             推荐内容：
             <a
               class="block"
               target="_blank"
-              href="https://github.com/Fridolph/Fridolph/blob/master/10%E5%85%B6%E4%BB%96/%E5%89%8D%E7%AB%AF%E5%AE%89%E5%85%A8%E5%AE%9E%E6%88%98/0%E4%BD%A0%E7%9C%9F%E7%9A%84%E4%BA%86%E8%A7%A3EDR%E5%90%97.md"
-              >安全-你真的了解EDR吗</a
-            >
-            <a
-              class="block"
-              target="_blank"
-              href="https://github.com/Fridolph/Fridolph/blob/master/02CSS%E7%9B%B8%E5%85%B3/%E5%93%8D%E5%BA%94%E5%BC%8F%E5%B8%83%E5%B1%80/%E5%93%8D%E5%BA%94%E8%AE%BE%E8%AE%A1.md"
-              >学习响应式设计</a
+              href="https://www.xiaoayu.ren/yandaowang/"
+              >研导博客上线</a
             >
           </div>
           <a
             class="flex items-center justify-center p-4 cursor-pointer"
-            href="https://github.com/Fridolph/Fridolph"
+            href="https://rate.xiaoayu.ren"
             target="_blank">
             <Iconfont
               name="tiaozhuan"
@@ -36,15 +30,12 @@
       </RepositoryItem>
 
       <RepositoryItem
-        name="my-resume"
+        name="ChatGPT-Web"
         transitionType="formLeft"
-        description="在线简历，Vite + TailWindCSS 极速搭建"
-        link="https://github.com/Fridolph/my-resume/">
+        description="一个轻量的ChatGPT商用系统。"
+        link="https://aichat.xiaoayu.ren">
         <div class="flex flex-row">
           <div class="basis-full flex flex-wrap flex-row gap-1">
-            <img
-              alt="Static Badge"
-              src="https://img.shields.io/badge/%E8%84%9A%E6%89%8B%E6%9E%B6-vite-red" />
             <img
               alt="Static Badge"
               src="https://img.shields.io/badge/%E5%89%8D%E7%AB%AF%E6%A1%86%E6%9E%B6-Vue3-%236495ed" />
@@ -60,7 +51,7 @@
           </div>
           <a
             class="flex items-center justify-center p-4 cursor-pointer"
-            href="https://github.com/Fridolph/Fridolph"
+            href="https://aichat.xiaoayu.ren"
             target="_blank">
             <Iconfont
               name="tiaozhuan"
@@ -70,29 +61,23 @@
       </RepositoryItem>
 
       <RepositoryItem
-        name="my-program"
-        description="demo仓库，较为简单的CSS、JS效果的细节实现"
+        name="NJFU-WiFi-Tools"
+        description="南京林业大学校园网逆向工具"
         transitionType="fromTop"
-        link="https://github.com/Fridolph/my-program">
+        link="https://github.com/Sunhaha520/NJFU-WiFi-Tools">
         <div class="flex flex-row">
           <div class="flex-1">
             推荐内容：
             <a
               class="block"
               target="_blank"
-              href="https://fridolph.github.io/my-program/public/hover-transition/transform-effect/index.html"
-              >CSS3 Transform 常见效果一览</a
-            >
-            <a
-              class="block"
-              target="_blank"
-              href="https://fridolph.github.io/my-program/public/hover-transition/text-hover/index.html"
-              >CSS3 text-hover demo</a
+              href=""
+              >校园网工具使用教程</a
             >
           </div>
           <a
             class="flex items-center justify-center p-4 cursor-pointer"
-            href="https://github.com/Fridolph/Fridolph"
+            href="https://github.com/Sunhaha520/NJFU-WiFi-Tools"
             target="_blank">
             <Iconfont
               name="tiaozhuan"
@@ -102,10 +87,10 @@
       </RepositoryItem>
 
       <RepositoryItem
-        name="my-element-plus"
-        description="Vue3 + Vite + TS4 打造自己的组件库"
+        name="Smart-Search"
+        description="支持新搜索api的Ai搜索引擎，二次开发"
         transitionType="fromRight"
-        link="https://github.com/Fridolph/my-program">
+        link="https://github.com/Sunhaha520/Smart-Search">
         <div class="flex flex-row">
           <div class="basis-full flex flex-wrap flex-row gap-1">
             <img
@@ -124,7 +109,7 @@
           </div>
           <a
             class="flex items-center justify-center p-4 cursor-pointer"
-            href="https://github.com/Fridolph/Fridolph"
+            href="https://github.com/Sunhaha520/Smart-Search"
             target="_blank">
             <Iconfont
               name="tiaozhuan"
@@ -134,22 +119,21 @@
       </RepositoryItem>
 
       <RepositoryItem
-        name="FE-prepare-interview"
+        name="Machine-Vision-Project"
         transitionType="fadeIn"
-        description="前端面试综合指导大纲，根据个人理解二次加工"
-        link="https://github.com/Fridolph/FE-prepare-interview">
+        description="一些自学计算机视觉的项目"
+        link="https://github.com/Sunhaha520/Machine-Vision-Project">
         <div class="flex flex-row">
           <div class="flex flex-wrap gap-y-2">
-            <Button size="small">Vue源码</Button>
-            <Button size="small">Promise</Button>
-            <Button size="small">ES规范</Button>
-            <Button size="small">http</Button>
-            <Button size="small">项目经验</Button>
-            <Button size="small">性能优化</Button>
+            <Button size="small">Ai隔空键盘</Button>
+            <Button size="small">手距测试</Button>
+            <Button size="small">空闲车位监测</Button>
+            <Button size="small">人脸识别打卡</Button>
+            <Button size="small">车辆监测</Button>
           </div>
           <a
             class="flex items-center justify-center p-4 cursor-pointer"
-            href="https://github.com/Fridolph/FE-prepare-interview"
+            href="https://github.com/Sunhaha520/Machine-Vision-Project"
             target="_blank">
             <Iconfont
               name="tiaozhuan"
