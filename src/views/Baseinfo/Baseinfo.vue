5fb5baecbafa0385fcae7f971244879b6acc8ec2
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-03-20 23:19:02 +0800

    Update Baseinfo.vue

--- src/views/Baseinfo/Baseinfo.vue
+++ src/views/Baseinfo/Baseinfo.vue
@@ -4,20 +4,20 @@
     icon="mingpian">
     <RowContent
       label="姓名"
-      text="付寅生" />
+      text="孙振宇" />
     <RowContent
       label="学历 / 专业"
-      text="本科 / 通信工程" />
+      text="本科(硕士在读) / 土木工程" />
     <RowContent
       label="工作经验"
-      text="八年" />
+      text="还在艰难实习" />
     <RowContent
       label="所在地"
-      text="成都市高新区" />
+      text="江苏连云港" />
 
     <RowContent
-      label="求职意向"
-      text="前端工程师 / 偏Vue" />
+      label="个人网站"
+      text="https://www.xiaoayu.ren" />
   </Section>
 </template>
 
