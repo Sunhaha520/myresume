c35e575e19ad417185d4661884bda0ac89ec6a32
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-03-21 00:27:57 +0800

    Update index.vue

--- src/views/Experience/company-wskp/index.vue
+++ src/views/Experience/company-wskp/index.vue
@@ -1,12 +1,12 @@
 <template>
   <RowContent
     align="right"
-    label="成都网思科平科技有限公司"
+    label="南京稀途信息技术有限公司"
     :label-bold="true"
     :label-width="220"
     :is-wrap="true"
     :is-title="true"
-    text="2017年7月——2024年1月" />
+    text="2023年3月——2023年6月" />
 
   <ListWrap
     title="工作内容与职责："
@@ -31,12 +31,8 @@ import ProLarge from './ProLarge.vue'
 import ProTeam from './ProTeam.vue'
 
 const list = ref([
-  '1. 负责项目前端架构搭建，确定技术框架，明确团队开发规范；',
-  '2. 整理产品需求文档，编写开发文档；',
-  '3. 配合设计人员进行前端开发，完成产品与客户的需求与功能；',
-  '4. 与后端工程师共同协商约定数据接口，完成前后台数据交互；',
-  '5. 完成项目相关需求开发、自测和Code Review，及时反馈测试相关问题；',
-  '6. 团队Leader，团队建设与资源共享。',
+  '1. 海外平台 (Facebook) 运营，负责广告投放，优化广告投放策略，提升产品及品牌曝光率；',
+  '2. 负责公司英文网站的日常维护及运营，提升英文网站的 PV、UV、leads 及 MQL 数量。',
 ])
 </script>
 
