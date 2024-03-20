cba3fcf655f5e76902474dafb57f05e5db1575bb
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-03-21 01:09:06 +0800

    Update Experience.vue

--- src/views/Experience/Experience.vue
+++ src/views/Experience/Experience.vue
@@ -5,12 +5,10 @@
     icon="gongzuoxinxi">
     <CompanyWskp />
     <div class="divide flex my-3"></div>
-    <CompanyAili />
   </Section>
 </template>
 
 <script setup lang="ts">
 import Section from '../../components/Layouts/Section.vue'
 import CompanyWskp from './company-wskp/index.vue'
-import CompanyAili from './company-aili/index.vue'
 </script>
